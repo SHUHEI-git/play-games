e382f1bad0e6ab6d2a1672e8e8d2bc0f23a041f9
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-02 09:38:48 +0900

    edit、update、destroyアクションを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
@@ -13,11 +14,27 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
   end
 
   private
   def post_params
     params.require(:post).permit(:title, :body, :invitation, :image)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
