425b140ea92221d014b26b4363829684eaf3d6dc
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-15 11:15:21 +0900

    cssとcontrollerの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @name = current_user.name
-    @posts = current_user.posts
+    @posts = @user.posts
   end
 end
