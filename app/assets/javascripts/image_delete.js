f16f3ae29e36287ae045a6c712c48ad2f19a64eb
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-16 10:16:32 +0900

    画像ファイルアップロード機能の実装1

---
+++ app/assets/javascripts/image_delete.js
@@ -0,0 +1,9 @@
+$(function(){
+  bsCustomFileInput.init();
+
+  document.getElementById('inputFileReset').addEventListener('click', function() {
+    var elem = document.getElementById('inputFile');
+    elem.value = '';
+    elem.dispatchEvent(new Event('change'));
+  });
+});
