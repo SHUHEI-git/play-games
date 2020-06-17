fd65b236fba194fc5413ad0aa0847dceb5917458
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-17 13:52:55 +0900

    commentモデルの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { maximum: 1000 }
 
   belongs_to :user
+  has_many :comments
 
   mount_uploader :image, ImageUploader
 end
