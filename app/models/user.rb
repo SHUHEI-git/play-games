f1b364d150e306f705f4ddbaa720f94a93198066
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-20 11:24:44 +0900

    controllerとmodelの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,12 @@ class User < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
+
   def remember_me
     true
   end
