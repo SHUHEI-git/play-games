c3951d399fa3c615a453fe65be421b6940ae61d7
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-20 13:19:28 +0900

    guestlogin設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :check_guest, only: [:update, :destroy]
 
   # GET /resource/sign_up
   # def new
@@ -37,6 +38,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def cancel
   #   super
   # end
+
+  def check_guest
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーは変更・削除はできません。'
+    end
+  end
 
   protected
 
