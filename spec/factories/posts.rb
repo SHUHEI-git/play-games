82ea4f68e23fd79675c0e07afc8b55f0bae013f6
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-21 21:56:29 +0900

    posts.rbの修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -13,8 +13,10 @@ FactoryBot.define do
     title { 'hello!' }
     body { 'hello!募集中' }
     invitation { '12345678' }
-    image Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/text_image.jpg'))
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/text_image.jpg')) }
     created_at { '2016-01-01T00:00:00Z' }
     updated_at { '2016-01-02T00:00:00Z' }
+    user_id { 1 }
+    user
   end
 end
