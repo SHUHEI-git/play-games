42479c78acb03aac33875346d09e5e8b0655f97e
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-21 20:41:17 +0900

    user_specの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,31 +10,31 @@ RSpec.describe User, type: :model do
     it 'nameが空では登録できないこと' do
       user = build(:user, name: nil)
       user.valid?
-      expect(user.errors[:name]).to include("can't be blank")
+      expect(user.errors[:name]).to include("を入力してください")
     end
 
     it 'emailが空では登録できないこと' do
       user = build(:user, email: nil)
       user.valid?
-      expect(user.errors[:email]).to include("can't be blank")
+      expect(user.errors[:email]).to include("を入力してください")
     end
 
     it 'passwordが空では登録できないこと' do
       user = build(:user, password: nil)
       user.valid?
-      expect(user.errors[:password]).to include("can't be blank")
+      expect(user.errors[:password]).to include("を入力してください")
     end
 
     it 'passwordが存在してもpassword_confirmationが空では登録できないこと' do
       user = build(:user, password_confirmation: '')
       user.valid?
-      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
     end
 
     it 'nameが9文字以上であれば登録できないこと' do
       user = build(:user, name: 'aaaaaaaaa')
       user.valid?
-      expect(user.errors[:name]).to include("is too long (maximum is 8 characters)")
+      expect(user.errors[:name]).to include("は8文字以内で入力してください")
     end
 
     it 'nameが8文字以下では登録できること' do
@@ -46,7 +46,7 @@ RSpec.describe User, type: :model do
       user = create(:user)
       another_user = build(:user, email: user.email)
       another_user.valid?
-      expect(another_user.errors[:email]).to include("has already been taken")
+      expect(another_user.errors[:email]).to include("はすでに存在します")
     end
 
     it "passwordが6文字以上であれば登録できること" do
@@ -58,7 +58,7 @@ RSpec.describe User, type: :model do
     it "passwordが5文字以下であれば登録できないこと" do
       user = build(:user, password: "00000", password_confirmation: "00000")
      user.valid?
-     expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+     expect(user.errors[:password]).to include("は6文字以上で入力してください")
     end
 
   end
