7b18e4993f8b0929147b316a59cf34e8dfaabe7d
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-06-22 10:30:46 +0900

    add comments.rbを記述

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context '保存できる場合' do
+      it 'textが存在すれば保存できること' do
+        
+      end
+    end
+  end
 end
