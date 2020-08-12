057277f8255d4dcefac2e6d9587a432f72f6c7f4
SHUHEI-11 <ngsarteirantou@yahoo.co.jp>
2020-08-12 14:08:18 +0900

    create model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :liked_posts, through: :likes, source: :post
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   validates :name, presence: true, length: { maximum: 8 }
 
@@ -21,5 +25,17 @@ class User < ApplicationRecord
 
   def remember_me
     true
+  end
+
+  def following?(other_user)
+    following_relationships.find_by(following_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    following_relationships.create!(following_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    following_relationships.find_by(following_id: other_user.id).destroy
   end
 end
